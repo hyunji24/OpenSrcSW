82c8dacf09cbade42b155476680466116b1cceeb
hyunji <24hyunji@naver.com>
2021-04-10 15:21:08 +0900

    searcher

---
+++ git01/src/git01/searcher.java
@@ -0,0 +1,12 @@
+package git01;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class searcher {
+
+	static double CalcSim(HashMap<String,List<Double>> hashMap,int i) {
+		return i;
+		
+	}
+}
